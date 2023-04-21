bffe1186fca00a3f88edda9872f9d85f4883993f
hbcode1 <houcine.bh@pm.me>
2023-04-21 00:47:11 +0000

    adding test suite for task 3

---
+++ check_0.3.sh
@@ -0,0 +1,102 @@
+#!/bin/bash
+
+# Test case for "ls" command
+command1=$(echo "ls" | ./shell)
+command2=$(echo "ls" | sh)
+echo "$command1" > 1a2b3c4d5e6f7g8h9i
+echo "$command2" > 1a2b3c4d5e6f7g8h9b
+if diff 1a2b3c4d5e6f7g8h9i 1a2b3c4d5e6f7g8h9b > /dev/null; then
+    echo -e "\e[32mSuccess\e[0m"
+else
+    echo -e "\e[31mFailed\e[0m"
+    diff 1a2b3c4d5e6f7g8h9i 1a2b3c4d5e6f7g8h9b
+fi
+rm 1a2b3c4d5e6f7g8h9i 1a2b3c4d5e6f7g8h9b;
+read -p "Next?"
+echo "************************************"
+# Test case for "/bin/ls"
+command1=$(echo "/bin/ls" | ./shell)
+command2=$(echo "/bin/ls" | sh)
+echo "$command1" > 1a2b3c4d5e6f7g8h9i
+echo "$command2" > 1a2b3c4d5e6f7g8h9b
+if diff 1a2b3c4d5e6f7g8h9i 1a2b3c4d5e6f7g8h9b >/dev/null; then
+    echo -e "\e[32mSuccess\e[0m"
+else
+    echo -e "\e[31mFailed\e[0m"
+    diff 1a2b3c4d5e6f7g8h9i 1a2b3c4d5e6f7g8h9b
+fi
+rm 1a2b3c4d5e6f7g8h9i 1a2b3c4d5e6f7g8h9b;
+read -p "Next?"
+echo "************************************"
+# Test case for "ls" command with "-l" option
+command1=$(echo "/bin/ls -l" | ./shell)
+command2=$(echo "/bin/ls -l" | sh)
+echo "$command1" > 1a2b3c4d5e6f7g8h9i
+echo "$command2" > 1a2b3c4d5e6f7g8h9b
+if diff 1a2b3c4d5e6f7g8h9i 1a2b3c4d5e6f7g8h9b >/dev/null; then
+    echo -e "\e[32mSuccess\e[0m"
+else
+    echo -e "\e[31mFailed\e[0m"
+    diff 1a2b3c4d5e6f7g8h9i 1a2b3c4d5e6f7g8h9b
+fi
+rm 1a2b3c4d5e6f7g8h9i 1a2b3c4d5e6f7g8h9b;
+read -p "Next?"
+echo "************************************"
+# Test case for "ls" command with "-l" and "/tmp" option
+command1=$(echo "/bin/ls -l /tmp" | ./shell)
+command2=$(echo "/bin/ls -l /tmp" | sh)
+echo "$command1" > 1a2b3c4d5e6f7g8h9i
+echo "$command2" > 1a2b3c4d5e6f7g8h9b
+if diff 1a2b3c4d5e6f7g8h9i 1a2b3c4d5e6f7g8h9b >/dev/null; then
+    echo -e "\e[32mSuccess\e[0m"
+else
+    echo -e "\e[31mFailed\e[0m"
+    diff 1a2b3c4d5e6f7g8h9i 1a2b3c4d5e6f7g8h9b
+fi
+rm 1a2b3c4d5e6f7g8h9i 1a2b3c4d5e6f7g8h9b;
+read -p "Next?"
+echo "************************************"
+# Test case for "echo" command
+command1=$(echo "echo /bin/ls" | ./shell)
+command2=$(echo "echo /bin/ls" | sh)
+echo "$command1" > 1a2b3c4d5e6f7g8h9i
+echo "$command2" > 1a2b3c4d5e6f7g8h9b
+if diff 1a2b3c4d5e6f7g8h9i 1a2b3c4d5e6f7g8h9b >/dev/null; then
+    echo -e "\e[32mSuccess\e[0m"
+else
+    echo -e "\e[31mFailed\e[0m"
+    diff 1a2b3c4d5e6f7g8h9i 1a2b3c4d5e6f7g8h9b
+fi
+rm 1a2b3c4d5e6f7g8h9i 1a2b3c4d5e6f7g8h9b;
+read -p "Next?"
+echo "************************************"
+# Test case for "cat" command
+echo -e "This is a\n text" > impossible_name.abcd
+command1=$(echo "cat text" | ./shell)
+command2=$(echo "cat text" | sh)
+echo "$command1" > 1a2b3c4d5e6f7g8h9i
+echo "$command2" > 1a2b3c4d5e6f7g8h9b
+if diff 1a2b3c4d5e6f7g8h9i 1a2b3c4d5e6f7g8h9b >/dev/null; then
+    echo -e "\e[32mSuccess\e[0m"
+else
+    echo -e "\e[31mFailed\e[0m"
+    diff 1a2b3c4d5e6f7g8h9i 1a2b3c4d5e6f7g8h9b
+fi
+rm -f impossible_name.abcd
+rm 1a2b3c4d5e6f7g8h9i 1a2b3c4d5e6f7g8h9b;
+read -p "Next?"
+echo "************************************"
+# Test case for "exit" command
+command1=$(echo "exit" | ./shell)
+command2=$(echo "./shell: No such file or directory")
+echo "$command1" > 1a2b3c4d5e6f7g8h9i
+echo "$command2" > 1a2b3c4d5e6f7g8h9b
+if diff 1a2b3c4d5e6f7g8h9i 1a2b3c4d5e6f7g8h9b >/dev/null; then
+    echo -e "\e[32mSuccess\e[0m"
+else
+    echo -e "\e[31mFailed\e[0m"
+    diff 1a2b3c4d5e6f7g8h9i 1a2b3c4d5e6f7g8h9b
+fi
+rm 1a2b3c4d5e6f7g8h9i 1a2b3c4d5e6f7g8h9b;
+read -p "Next?"
+
