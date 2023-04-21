dd85a6a745409f11b28be5ed3f2cd6fffddc0388
hbcode1 <houcine.bh@pm.me>
2023-04-21 00:46:13 +0000

    adding test suite for task 1

---
+++ check_0.1.sh
@@ -0,0 +1,72 @@
+#!/bin/bash
+# Test case for "Ctrl+D" command
+command1=$(echo -e "\n" | ./shell)
+command2=$(echo -e "\n" | sh)
+echo "$command1" > 1a2b3c4d5e6f7g8h9i
+echo "$command2" > 1a2b3c4d5e6f7g8h9b
+if diff 1a2b3c4d5e6f7g8h9i 1a2b3c4d5e6f7g8h9b >/dev/null; then
+    echo -e "\e[32mSuccess\e[0m"
+else
+    echo -e "\e[31mFailed\e[0m"
+    diff 1a2b3c4d5e6f7g8h9i 1a2b3c4d5e6f7g8h9b
+fi
+rm 1a2b3c4d5e6f7g8h9i 1a2b3c4d5e6f7g8h9b;
+read -p "Next?"
+echo "************************************"
+# Test case for "ls" command
+command1=$(echo "ls" | ./shell)
+command2=$(echo "./shell: No such file or directory")
+echo "$command1" > 1a2b3c4d5e6f7g8h9i
+echo "$command2" > 1a2b3c4d5e6f7g8h9b
+if diff 1a2b3c4d5e6f7g8h9i 1a2b3c4d5e6f7g8h9b > /dev/null; then
+    echo -e "\e[32mSuccess\e[0m"
+else
+    echo -e "\e[31mFailed\e[0m"
+    diff 1a2b3c4d5e6f7g8h9i 1a2b3c4d5e6f7g8h9b
+fi
+rm 1a2b3c4d5e6f7g8h9i 1a2b3c4d5e6f7g8h9b;
+read -p "Next?"
+echo "************************************"
+# Test case for "/bin/ls" command with no arguments
+command1=$(echo "/bin/ls" | ./shell)
+command2=$(echo "/bin/ls" | sh)
+echo "$command1" > 1a2b3c4d5e6f7g8h9i
+echo "$command2" > 1a2b3c4d5e6f7g8h9b
+if diff 1a2b3c4d5e6f7g8h9i 1a2b3c4d5e6f7g8h9b >/dev/null; then
+    echo -e "\e[32mSuccess\e[0m"
+else
+    echo -e "\e[31mFailed\e[0m"
+    diff 1a2b3c4d5e6f7g8h9i 1a2b3c4d5e6f7g8h9b
+fi
+rm 1a2b3c4d5e6f7g8h9i 1a2b3c4d5e6f7g8h9b;
+read -p "Next?"
+echo "************************************"
+# Test case for "/bin/ls" command with "-l" option
+command1=$(echo "/bin/ls -l" | ./shell)
+command2=$(echo "./shell: No such file or directory")
+echo "$command1" > 1a2b3c4d5e6f7g8h9i
+echo "$command2" > 1a2b3c4d5e6f7g8h9b
+if diff 1a2b3c4d5e6f7g8h9i 1a2b3c4d5e6f7g8h9b >/dev/null; then
+    echo -e "\e[32mSuccess\e[0m"
+else
+    echo -e "\e[31mFailed\e[0m"
+    diff 1a2b3c4d5e6f7g8h9i 1a2b3c4d5e6f7g8h9b
+fi
+rm 1a2b3c4d5e6f7g8h9i 1a2b3c4d5e6f7g8h9b;
+read -p "Next?"
+echo "************************************"
+# Test case for "exit" command
+command1=$(echo "exit" | ./shell)
+command2=$(echo "./shell: No such file or directory")
+echo "$command1" > 1a2b3c4d5e6f7g8h9i
+echo "$command2" > 1a2b3c4d5e6f7g8h9b
+if diff 1a2b3c4d5e6f7g8h9i 1a2b3c4d5e6f7g8h9b >/dev/null; then
+    echo -e "\e[32mSuccess\e[0m"
+else
+    echo -e "\e[31mFailed\e[0m"
+    diff 1a2b3c4d5e6f7g8h9i 1a2b3c4d5e6f7g8h9b
+fi
+rm 1a2b3c4d5e6f7g8h9i 1a2b3c4d5e6f7g8h9b;
+read -p "Next?"
+
+
