8f366ffd9392f14e5933e42b3d584b69bb7af741
hbcode1 <houcine.bh@pm.me>
2023-04-25 19:28:00 +0100

    add some edge cases

--- check.sh
+++ check.sh
@@ -4,7 +4,7 @@
 gcc -Wall -Werror -Wextra -pedantic -std=gnu89 *.c -o hsh
 function run_check()
 {
-    echo "-->" $1
+    echo "--> [" $1 "] <--"
     chars1=$(wc -m < 1a2b3c4d5e6f7g8h9i)
     chars2=$(wc -m < 1a2b3c4d5e6f7g8h9b)
     if diff 1a2b3c4d5e6f7g8h9i 1a2b3c4d5e6f7g8h9b >/dev/null; then
@@ -25,48 +25,70 @@ command1=$(echo "ls" | ./hsh 2>&1)
 command2=$(echo "ls" | sh 2>&1)
 echo "$command1" > 1a2b3c4d5e6f7g8h9i
 echo "$command2" > 1a2b3c4d5e6f7g8h9b
-run_check "Test case for "ls" command"
+run_check ""ls""
 command1=$(echo "/bin/ls" | ./hsh 2>&1)
 command2=$(echo "/bin/ls" | sh 2>&1)
 echo "$command1" > 1a2b3c4d5e6f7g8h9i
 echo "$command2" > 1a2b3c4d5e6f7g8h9b
-run_check "Test case for "/bin/ls""
+run_check ""/bin/ls""
 command1=$(echo "/bin/ls -l" | ./hsh 2>&1)
 command2=$(echo "/bin/ls -l" | sh 2>&1)
 echo "$command1" > 1a2b3c4d5e6f7g8h9i
 echo "$command2" > 1a2b3c4d5e6f7g8h9b
-run_check "Test case for "ls" command with "-l" option"
+run_check ""ls" with "-l" option"
 command1=$(echo "/bin/ls -l /tmp" | ./hsh 2>&1)
 command2=$(echo "/bin/ls -l /tmp" | sh 2>&1)
 echo "$command1" > 1a2b3c4d5e6f7g8h9i
 echo "$command2" > 1a2b3c4d5e6f7g8h9b
-run_check "Test case for "ls" command with "-l" and "/tmp" option"
+run_check ""ls" with "-l" and "/tmp" option"
 command1=$(echo "echo /bin/ls" | ./hsh 2>&1)
 command2=$(echo "echo /bin/ls" | sh 2>&1)
 echo "$command1" > 1a2b3c4d5e6f7g8h9i
 echo "$command2" > 1a2b3c4d5e6f7g8h9b
-run_check "Test case for "echo" command"
-# Test case for "cat" command
+run_check ""echo""
 echo -e "This is a\n text" > impossible_name.abcd
 command1=$(echo "cat impossible_name.abcd" | ./hsh 2>&1)
 command2=$(echo "cat impossible_name.abcd" | sh 2>&1)
 echo "$command1" > 1a2b3c4d5e6f7g8h9i
 echo "$command2" > 1a2b3c4d5e6f7g8h9b
-run_check "Test case for "cat" command"
+run_check ""cat""
 rm -f impossible_name.abcd
-# Test case for "exit" command
 echo -e "This is a\n text" > impossible_name.abcd
 command1=$(echo "cat impossible_name.abcd" | ./hsh 2>&1)
 command2=$(echo "cat impossible_name.abcd" | sh 2>&1)
 echo "$command1" > 1a2b3c4d5e6f7g8h9i
 echo "$command2" > 1a2b3c4d5e6f7g8h9b
-run_check "# Test case for "exit" command"
+run_check "# "exit""
 rm -f impossible_name.abcd
-command1=$(echo "env" | ./hsh 2>&1)
-command2=$(echo "env" | sh 2>&1)
+command1=$(echo "./ls" | ./hsh 2>&1)
+command2=$(echo "./ls" | sh 2>&1)
 echo "$command1" > 1a2b3c4d5e6f7g8h9i
 echo "$command2" > 1a2b3c4d5e6f7g8h9b
-run_check "Test case for env"
+run_check "./ls"
+command1=$(echo "." | ./hsh 2>&1)
+command2=$(echo "." | sh 2>&1)
+echo "$command1" > 1a2b3c4d5e6f7g8h9i
+echo "$command2" > 1a2b3c4d5e6f7g8h9b
+run_check "."
+command1=$(echo "BLAbla" | ./hsh 2>&1)
+echo "$command1" > 1a2b3c4d5e6f7g8h9i
+echo "./hsh: 1: BLAbla: not found" > 1a2b3c4d5e6f7g8h9b
+run_check "command not found"
+command1=$(echo -e "\n" | ./hsh 2>&1)
+command2=$(echo -e "\n" | sh 2>&1)
+echo "$command1" > 1a2b3c4d5e6f7g8h9i
+echo "$command2" > 1a2b3c4d5e6f7g8h9b
+run_check "\n"
+command1=$(echo -e "                        " | ./hsh 2>&1)
+command2=$(echo -e "                        " | sh 2>&1)
+echo "$command1" > 1a2b3c4d5e6f7g8h9i
+echo "$command2" > 1a2b3c4d5e6f7g8h9b
+run_check "          <spaces>              "
+#command1=$(echo "env" | ./hsh 2>&1)
+#command2=$(echo "env" | sh 2>&1)
+#echo "$command1" > 1a2b3c4d5e6f7g8h9i
+#echo "$command2" > 1a2b3c4d5e6f7g8h9b
+#run_check "env"
 echo "-->" Check documentation
 betty-doc *.c *.h
 echo "-->" Check style
